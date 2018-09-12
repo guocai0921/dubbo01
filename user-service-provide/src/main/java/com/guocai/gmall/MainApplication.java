0537e054ac6d4697ee8c6a00f9535c5d5fb49b06
guocai0921 <923055433@qq.com>
2018-09-12 16:53:42 +0800

    v1.0.0

---
+++ user-service-provide/src/main/java/com/guocai/gmall/MainApplication.java
@@ -0,0 +1,16 @@
+package com.guocai.gmall;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class MainApplication {
+
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ((ClassPathXmlApplicationContext) context).start();
+        System.in.read();
+    }
+
+}
